d0adcb66cde0dd22784c8f9ae77c4da256865cc3
sachin8935 <sachin89359@gmail.com>
2023-12-21 05:08:14 +0530

    front_end_updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./component/NavBar";
-import About from './component/About';
+import About from "./component/About";
 import Footer from "./component/Footer";
-import Events from './component/Events';
+import Events from "./component/Events";
 import Landing from "./component/Landing";
 import Achievements from "./component/Achievements";
 import Partner from "./component/Partner";
+import axios from "axios";
 
 const App = () => {
+  useEffect(() => {
+    (async () => {
+      try {
+        const events = await axios.get(
+          "http://localhost:3000/cac/v1/getOngoingEvents/all"
+        );
+        const achivments = await axios.get(
+          "http://localhost:3000/cac/v1//getAchivments/all"
+        );
+        console.log(events.data);
+        console.log(achivments.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    })();
+  }, []);
+
   return (
     <div className="bg-white">
-      <NavBar/>
-      <Landing/>
-      <About/>
-      <Achievements/>
-      <Events/>
-      <Partner/>
-      <Footer/>
+      <NavBar />
+      <Landing />
+      <About />
+      <Achievements />
+      <Events />
+      <Partner />
+      <Footer />
     </div>
   );
 };
