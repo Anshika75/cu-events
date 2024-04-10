9c5f9967123934f866c89854f5ff5c19bc19fecb
Anshika <anshikathakur596@gmail.com>
2024-04-10 22:12:30 +0530

    Update Landing.jsx

--- src/component/Landing.jsx
+++ src/component/Landing.jsx
@@ -6,32 +6,43 @@ import "./styles.css";
 import { Autoplay } from "swiper/modules";
 import { FaLongArrowAltRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { projectFirestore } from "../firebase";
 
 const LandingBanner = () => {
+  const [bannerData, setBannerData] = useState([]);
 
+  useEffect(() => {
+    const fetchBannerData = async () => {
+      try {
+        const bannerCollection = await projectFirestore.collection('banner').get();
+        const bannerData = bannerCollection.docs.map(doc => doc.data());
+        setBannerData(bannerData);
+      } catch (error) {
+        console.error("Error fetching banner data:", error);
+      }
+    };
+
+    fetchBannerData();
+  }, []);
   return (
     <>
       <Swiper
         id={"home"}
         centeredSlides={true}
         autoplay={{
-          delay: 25000,
+          delay: 37000,
           disableOnInteraction: false,
         }}
         modules={[Autoplay]}
         className="mySwiper w-full h-[30vh] md:h-[60vh] lg:h-[100vh]"
       >
-          
-          <SwiperSlide className="relative w-full h-full flex flex-col swiper-slide2">
+           {bannerData.map((banner, index) => (
+          <SwiperSlide key={index} className="relative w-full h-full flex flex-col swiper-slide1">
             <video loop muted autoPlay controls = '' className="w-full h-full object-cover">
-              <source src="https://www.dropbox.com/scl/fi/j4dpes26gdbi4f6onia9z/Chandigarh-University-wishes-you-all-a-happy-Holi.mp4?rlkey=jwolitqncublfdt583pcm2f4e&dl=1" type="video/mp4" />
+              <source src={banner.video} type="video/mp4" />
             </video>
           </SwiperSlide>
-          <SwiperSlide className="relative w-full h-full flex flex-col swiper-slide1">
-            <video loop muted autoPlay controls = '' className="w-full h-full object-cover">
-              <source src="https://www.dropbox.com/scl/fi/ubss7ym21rnenwalbs9vj/CU-FEST-Back-with-Bang.mp4?rlkey=n1pqgc11irhz8c8d99ie3xxkf&dl=1" type="video/mp4" />
-            </video>
-          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
