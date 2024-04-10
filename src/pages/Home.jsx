427cad20ef8434bacf25c16c7b8df5d8dc267571
Anshika <anshikathakur596@gmail.com>
2024-04-10 22:37:04 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from '../component/NavBar';
 import Landing from '../component/Landing';
 import Stats from '../component/Stats';
@@ -9,20 +9,45 @@ import Partner from '../component/Partner';
 import Footer from '../component/Footer';
 import StaticEvents from '../component/StaticEvent';
 import StaticAchievements from '../component/StaticAchievements';
+import Loader from '../component/loader';
 
-export default function Home() {
-    return (
+const Home = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      // Set a minimum timeout of 2000 milliseconds (2 seconds)
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    };
+    window.addEventListener('load', handleLoad);
+
+    return () => {
+      window.removeEventListener('load', handleLoad);
+    };
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
         <div className="bg-white">
-            <NavBar/>
-            <Landing id="home"/>
-            <Stats id="stats"/>
-            <About id="about"/>
-            {/* <Events id="events"/> */}
-            <StaticEvents id="sevents"/>
-            {/* <Achievements id="achievements"/> */}
-            <StaticAchievements id="sachievements"/>
-            <Partner id="partner"/>
-            <Footer id="contact"/>
+          <NavBar />
+          <Landing id="home" />
+          <Stats id="stats" />
+          <About id="about" />
+          {/* <Events id="events"/> */}
+          <StaticEvents id="sevents" />
+          {/* <Achievements id="achievements"/> */}
+          <StaticAchievements id="sachievements" />
+          <Partner id="partner" />
+          <Footer id="contact" />
         </div>
-    )
-}
+      )}
+    </>
+  );
+};
+
+export default Home;
