75306e5a0b1cf3232e2d9b5af3fef2f3b5f30eed
Anshika <anshikathakur596@gmail.com>
2024-05-01 18:33:02 +0530

    Update StaticAchievements.jsx

--- src/component/StaticAchievements.jsx
+++ src/component/StaticAchievements.jsx
@@ -24,7 +24,7 @@ const ModuleBox = ({ onClose, Name, Description, Image, Link }) => {
     }
   };
 
-  const limitedDescription = limitDescription(Description, 100);
+  const limitedDescription = limitDescription(Description, 80);
 
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#060414de] shadow-lg shadow-[#ffffff17] z-20 grid place-items-center w-[90%] h-[95%] lg:h-[90%]">
