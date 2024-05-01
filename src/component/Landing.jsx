d99d10814195c2ab8145a1f3afa9b501ed2cb755
Anshika <anshikathakur596@gmail.com>
2024-05-01 18:07:08 +0530

    Update Landing.jsx

--- src/component/Landing.jsx
+++ src/component/Landing.jsx
@@ -5,8 +5,9 @@ import "swiper/css/pagination";
 import "./styles.css";
 import { Autoplay } from "swiper/modules";
 import { FaLongArrowAltRight } from "react-icons/fa";
-import { Link } from "react-router-dom";
 import { projectFirestore } from "../firebase";
+import slide3 from "../assets/slide3.png";
+import { Link } from "react-router-dom";
 
 const LandingBanner = () => {
   const [bannerData, setBannerData] = useState([]);
@@ -24,28 +25,33 @@ const LandingBanner = () => {
 
     fetchBannerData();
   }, []);
+
   return (
     <>
       <Swiper
         id={"home"}
         centeredSlides={true}
         autoplay={{
-          delay: 37000,
+          delay: 2500,
           disableOnInteraction: false,
         }}
         modules={[Autoplay]}
         className="mySwiper w-full h-[30vh] md:h-[60vh] lg:h-[100vh]"
       >
-           {bannerData.map((banner, index) => (
+
+        {/* <SwiperSlide className="relative w-full h-full flex flex-col swiper-slide1">
+          <Link to="/hackentine" className="cursor-pointer w-full h-full absolute top-0 left-0 z-20"></Link>
+          <img className="w-full h-full object-cover" src={slide3} alt="" />
+        </SwiperSlide> */}
+
+        {bannerData.map((banner, index) => (
           <SwiperSlide key={index} className="relative w-full h-full flex flex-col swiper-slide1">
-            <video loop muted autoPlay controls = '' className="w-full h-full object-cover">
-              <source src={banner.video} type="video/mp4" />
-            </video>
+            <a href={banner.Link} target="_blank" className="cursor-pointer w-full h-full absolute top-0 left-0 z-20"></a>
+            <img className="w-full h-full object-cover" src={banner.Image} alt="" />
           </SwiperSlide>
         ))}
       </Swiper>
     </>
   );
-};
-
+}
 export default LandingBanner;
